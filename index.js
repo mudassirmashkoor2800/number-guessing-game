fab8da8f71f6cf4e0ca4ef20e5146901583cda0e
mudassirmashkoor2800 <mudassirmashkoor@gmail.com>
2024-03-25 23:56:37 +0500

    Created Number Guessing Game

---
+++ index.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([{
+        name: "userGuessNumber",
+        type: "number",
+        message: "Please Guess a Number 1 To 6"
+    }]);
+if (answers.userGuessNumber === randomNumber) {
+    console.log("Congratulations You Won The Game");
+}
+else {
+    console.log("You Guess Wrong Number");
+}
+;
